c4d1f833b02142ee68df7d474764bc92d2bbd5b6
merothh <merothh@gmail.com>
2018-05-04 18:11:12 +0000

    mido: mido_defconfig -> franco_mido-treble_defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Kernel
-TARGET_KERNEL_CONFIG := mido_defconfig
+TARGET_KERNEL_CONFIG := franco_mido-treble_defconfig
 
 # Partitions
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
